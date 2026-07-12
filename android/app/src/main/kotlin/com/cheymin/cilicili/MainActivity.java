671eb5f23b458c89ec4f1a4e6ba11c184762fe55
cheymin <m13608319315@163.com>
2026-07-12 05:28:00 +0000

    feat: 删除仓库内容
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ android/app/src/main/kotlin/com/cheymin/cilicili/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cheymin.cilicili;
+
+import io.flutter.embedding.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/cheymin/cilicili/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.cheymin.cilicili
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
