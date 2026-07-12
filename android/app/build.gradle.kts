4b57b10097c229cbdb0eeb251d3acaf27d5ce4d4
cheymin <m13608319315@163.com>
2026-07-12 07:55:06 +0000

    fix: 添加 java.util.Properties 导入修复 Kotlin DSL 编译错误

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,11 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-val localProperties = java.util.Properties()
+val localProperties = Properties()
 val localPropertiesFile = rootProject.file("local.properties")
 if (localPropertiesFile.exists()) {
     localPropertiesFile.inputStream().use { localProperties.load(it) }
